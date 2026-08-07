32af77ebec654603f4ced2e06c65508c4a349976
Shelley <shelley@exe.dev>
2026-08-07 14:32:53 +0000

    Clarify Markdown line break guidance
    Co-authored-by: Shelley <shelley@exe.dev>

--- srv/static/sw.js
+++ srv/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'songs-shell-v18';
+const CACHE = 'songs-shell-v19';
 const SHELL = ['/', '/static/style.css', '/static/app.js', '/static/icon.svg', '/manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
