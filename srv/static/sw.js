74dfbf755f55ce8de3a2c4428e69177f13b59337
exe.dev user <exedev@kgl-songs.exe.xyz>
2026-08-23 22:25:38 +0000

    Fit lead-sheet titles with song typography
    Co-authored-by: Shelley <shelley@exe.dev>

--- srv/static/sw.js
+++ srv/static/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'songs-shell-v39';
-const SHELL = ['/', '/static/style.css', '/static/style.css?v=20260823-12', '/static/app.js', '/static/app.js?v=20260823-10', '/static/icon.svg', '/manifest.webmanifest'];
+const CACHE = 'songs-shell-v40';
+const SHELL = ['/', '/static/style.css', '/static/style.css?v=20260823-13', '/static/app.js', '/static/app.js?v=20260823-10', '/static/icon.svg', '/manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(Promise.all([
   self.clients.claim(),
